926e0d15463da89152e8a3e32159d011fae6419e
PARDEEP KAUR <pkaur84@aol.com>
2020-05-13 06:41:56 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, 
+  attr_accessor :author, :page_count
   attr_reader :title, :genre
-  
-  genre = []
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -11,3 +11,10 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+  # creates the writer for genre and adds the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+end
